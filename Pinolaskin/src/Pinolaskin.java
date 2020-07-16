afdb6732e4e0716358a275823a3f10c847733744
elli kiiski <kiiski.elli@gmail.com>
2020-07-16 15:55:34 +0300

    pinolaskin lisäys

---
+++ Pinolaskin/src/Pinolaskin.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Pinolaskin {    
+    public long laske(String lasku) {
+        ArrayDeque<String> pino = new ArrayDeque<>();
+        String[] merkit = lasku.split(" ");
+        
+        for (int i = 0; i < merkit.length; i++) {
+            if (merkit[i].equals("+")) {
+                
+                long summa = Long.valueOf(pino.getFirst());
+                pino.removeFirst();
+                summa += Long.valueOf(pino.getFirst());
+                pino.removeFirst();
+                pino.addFirst("" + summa);
+            } else if (merkit[i].equals("*")) {
+                long tulo = Long.valueOf(pino.getFirst());
+                pino.removeFirst();
+                tulo *= Long.valueOf(pino.getFirst());
+                pino.removeFirst();
+                pino.addFirst("" + tulo);
+            } else {
+                pino.addFirst(merkit[i]);
+            }
+        }
+        long palauta = Long.parseLong(pino.getFirst());
+        return palauta;
+    }    
+}
